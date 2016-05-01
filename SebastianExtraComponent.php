11f0df70553ed82aa4b3c5f0b4749a37acc85457
Tyler Sebastian <tills13@gmail.com>
2016-04-30 23:52:35 -0700

    tabsto spaces

--- SebastianExtraComponent.php
+++ SebastianExtraComponent.php
@@ -1,35 +1,35 @@
 <?php
-	namespace SebastianExtra;
+    namespace SebastianExtra;
 
-	use Sebastian\Core\Component\Component;
-	use Sebastian\Core\Context\ContextInterface;
-	use Sebastian\Utility\Configuration\Configuration;
+    use Sebastian\Core\Component\Component;
+    use Sebastian\Core\Context\ContextInterface;
+    use Sebastian\Utility\Configuration\Configuration;
 
-	use SebastianExtra\EntityManager\EntityManager;
-	use SebastianExtra\Templating\SRender;
+    use SebastianExtra\EntityManager\EntityManager;
+    use SebastianExtra\Templating\SRender;
 
-	class SebastianExtraComponent extends Component {
-		public function __construct(ContextInterface $context, $name, Configuration $config = null) {
-			parent::__construct($context, $name, $config);
+    class SebastianExtraComponent extends Component {
+        public function __construct(ContextInterface $context, $name, Configuration $config = null) {
+            parent::__construct($context, $name, $config);
 
-			$this->setWeight(0);
-		}
+            $this->setWeight(0);
+        }
 
-		public function setup() {
-			$context = $this->getContext();
-			$config = $this->getConfig();
-			$components = $context->getComponents();
+        public function setup() {
+            $context = $this->getContext();
+            $config = $this->getConfig();
+            $components = $context->getComponents();
 
-			$context->templating = new SRender($this->getContext(), null, array_map(function($component) {
+            $context->templating = new SRender($this->getContext(), null, array_map(function($component) {
                 return $component->getResourceUri('views', true);
             }, $components));
 
             if ($config->get('orm.enabled', false)) {
-            	$context->entityManager = new EntityManager($context, $config->sub('orm', []));
+                $context->entityManager = new EntityManager($context, $config->sub('orm', []));
             }
-		}
+        }
 
-		public function checkRequirements(ContextInterface $context) {
-			return true;
-		}
-	}
+        public function checkRequirements(ContextInterface $context) {
+            return true;
+        }
+    }
